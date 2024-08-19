f3b990b5dcc0d1c0e2d5dc4577bd339dd6c737e6
Createll-3d <120663036+Createll-3d@users.noreply.github.com>
2024-08-20 00:02:12 +0800

    added new

---
+++ _createll_script_utils.py
@@ -0,0 +1,238 @@
+import bpy
+
+# probably need to break this down
+
+# Selection Utility
+def get_mesh_selection():
+    selected_objects = []
+    for obj in bpy.context.selected_objects:
+        if obj.type == 'MESH':
+            selected_objects.append(obj)
+    print(len(selected_objects))
+    return selected_objects
+
+def set_object_active(object):
+    if not bpy.context.window.scene in object.users_scene:
+        bpy.context.window.scene = object.users_scene[0]
+        
+    bpy.context.view_layer.objects.active = object
+    return object
+
+# Mesh Functions
+
+def apply_modifiers_till_modifier_type_via_gret(object, modifier_type):
+    index = 0
+    set_object_active(object)
+    object.select_set(True)
+    
+    for modifier in object.modifiers:
+        if modifier.type == modifier_type:
+            modifier_index = index
+            modifier_mask = tuple(modifier_index >= i for i in range(32))
+            try:
+                bpy.ops.gret.shape_key_apply_modifiers(modifier_mask=modifier_mask)
+                return
+            except:
+                raise print('Please Install Gret Add-on')
+        index += 1
+
+def apply_modifiers_via_gret(object):
+    if object and object.type == 'MESH':
+        set_object_active(object)
+        try:
+            bpy.ops.gret.shape_key_apply_modifiers()
+        except:
+            raise print('Please Install Gret Add-on')
+    return
+
+def clean_mesh_modifiers(object, modifier_names): # where we can input stuff we want to delete or adjusts
+    set_object_active(object)
+    remove_modifier_names = []
+
+    for modifier in object.modifiers: # doing it like this because reading while modifying produces memory targeting errors
+        if (modifier.name in modifier_names) or (modifier.type == 'NODES') or (not modifier.show_viewport) :
+            remove_modifier_names.append(modifier.name)
+    
+    for modifier_name in remove_modifier_names:
+        bpy.ops.object.modifier_remove(modifier = modifier_name)
+
+def remove_modifier_by_type(object, modifier_type_lists): # 'ARMATURE', 'SURFACE_DEFORM', 'NODES'
+    set_object_active(object)
+    remove_modifier_names = []
+
+    for modifier in object.modifiers: # doing it like this because reading while modifying produces memory targeting errors
+        if (modifier.type in modifier_type_lists) :
+            remove_modifier_names.append(modifier.name)
+    
+    for modifier_name in remove_modifier_names:
+        bpy.ops.object.modifier_remove(modifier = modifier_name)
+
+def remove_modifier_by_name(object, modifier_name): 
+    if object.modifiers.get(modifier_name).type == 'ARMATURE':
+        write_armature_data_to_custom_properties(object)
+    
+    set_object_active(object)
+    bpy.ops.object.modifier_remove(modifier = modifier_name)
+
+
+def create_surface_deform_modifier(object, target_object, mod_index = 0):
+    set_object_active(object)
+    
+    mod_index = min(mod_index, len(object.modifiers)-1)
+    
+    modifier = object.modifiers.new('SurfaceDeform', 'SURFACE_DEFORM')
+    
+    modifier.target = target_object
+    bpy.ops.object.modifier_move_to_index(modifier = modifier.name, index = mod_index)
+    bpy.ops.object.surfacedeform_bind(modifier = modifier.name)
+
+def duplicate_mesh_object(object, copy_suffix = '.duplicate', copy_collection = None, replace_mesh = False): #bpy.context.collection
+    if copy_collection is None:
+        copy_collection = object.users_collection[0]
+    
+    if object and object.type == 'MESH':
+        if replace_mesh or not bpy.data.objects.get(object.name + copy_suffix, False):
+            if bpy.data.objects.get(object.name + copy_suffix, False):
+                new_object = bpy.data.objects.get(object.name + copy_suffix, False)
+                if not new_object.data.shape_keys == None:
+                    new_object.shape_key_clear()
+                bpy.data.objects.remove(new_object)
+            
+            new_object = object.copy()
+            new_object.data = object.data.copy()
+            new_object.animation_data_clear()
+            new_object.name = object.name + copy_suffix
+            
+            copy_collection.objects.link(new_object)
+            return new_object
+        else:
+            new_obj = bpy.data.objects.get(object.name + copy_suffix)
+            link_object_to_collection(new_object, copy_collection)
+            return new_object
+    else:
+        print("Select a mesh object to duplicate.")
+
+
+# Object Functions
+
+
+def change_customProperty_name(object, new_name, old_name):
+    if old_name in object.keys():
+        object[new_name] = object[old_name]
+        del object[old_name]
+        
+
+def link_collections_to_scene(scene, collection_names_lists):
+    for collection_name in collection_names_lists:
+        try:
+            scene.collection.children.link(bpy.data.collections.get(collection_name))
+        except:
+            None
+            
+def link_object_to_collection(object, target_collection):
+    try:
+        for collection in object.users_collection:
+            collection.objects.unlink(object)
+        
+        target_collection.objects.link(object)
+    except:
+        None
+
+# Rig toggles
+
+def get_rig_type_count(mesh_lists = get_mesh_selection(), is_toggle = False):
+    rig_count = 0
+    for object in mesh_lists:
+        if object.modifiers.get('Armature', False):
+            if get_rig_name(object) == 'metarig':
+                rig_count += 1
+            else:
+                rig_count -= 1
+        else:
+            continue
+    if is_toggle:
+        rig_count_type = 'METARIG' if rig_count < 0 else 'ANIMRIG'
+    else:
+        rig_count_type = 'ANIMRIG' if rig_count < 0 else 'METARIG'
+    return rig_count_type
+
+def get_rig_name(object):
+    " Used for a secure rig state check "
+    if object.modifiers.get('Armature',False):
+        object['_rig_name'] = object.modifiers.get('Armature').object.name
+    elif object.get('_rig_name',False):
+        object['_rig_name'] = object['_rig_name']
+    else :
+        object['_rig_name'] = 'metarig'
+    return object['_rig_name']
+
+def get_modifier_index_by_name(object, modifier_name):
+    index = 0
+    for modifier in object.modifiers:
+        if modifier.name == modifier_name:
+            return index
+        index += 1
+    return None
+
+def create_armature_modifier(object, armature_object = None, mod_index = 0):
+
+    if armature_object is None:
+        armature_object = bpy.data.objects.get(object['_rig_name'])
+    
+    set_object_active(object)
+    mod_index = min(mod_index, len(object.modifiers))
+    modifier = object.modifiers.new('Armature', 'ARMATURE')
+
+    modifier.object = armature_object #bpy.data.objects['rig_anim']
+    bpy.ops.object.modifier_move_to_index(modifier = modifier.name, index = mod_index)
+    
+    write_armature_data_to_custom_properties(object, modifier.name)
+
+def write_armature_data_to_custom_properties(object, modifier_name = 'Armature'):
+    armature_modifier = object.modifiers.get(modifier_name, False)
+    if armature_modifier:
+        object['_rig_name'] = armature_modifier.object.name
+        object['_rig_mod_index'] = get_modifier_index_by_name(object, modifier_name)
+        print (f"For {object.name} - _rig_name = {object['_rig_name']} - _rig_mod_index = {object['_rig_mod_index']}")
+
+
+def set_armature_and_bone_name(rig_type, weight_armature = bpy.data.armatures['metarig'], anim_armature = bpy.data.armatures['rig_anim'], mesh_lists = get_mesh_selection()):
+    # rig_type = 'ANIMRIG' or 'METARIG'
+    standard_rigify_deform_bones = set(['spine', 'pelvis.L', 'pelvis.R', 'thigh.L', 'thigh.L.001', 'shin.L', 'shin.L.001', 'foot.L', 'toe.L', 'thigh.R', 'thigh.R.001', 'shin.R', 'shin.R.001', 'foot.R', 'toe.R', 'temple.L', 'temple.R', 'tongue', 'chin', 'chin.001', 'chin.L', 'chin.R', 'jaw', 'jaw.L.001', 'jaw.R.001', 'tongue.001', 'tongue.002', 'cheek.T.L', 'cheek.T.R', 'jaw.L', 'jaw.R', 'nose', 'nose.L', 'nose.R', 'cheek.B.L.001', 'cheek.B.R.001', 'cheek.B.L', 'cheek.B.R',  'nose.002', 'nose.001', 'nose.003', 'nose.004', 'nose.L.001', 'nose.R.001', 'cheek.T.L.001', 'cheek.T.R.001', 'shoulder.L', 'upper_arm.L', 'upper_arm.L.001', 'forearm.L', 'forearm.L.001', 'hand.L', 'f_index.01.L', 'f_index.02.L', 'f_index.03.L', 'thumb.01.L', 'thumb.02.L', 'thumb.03.L', 'palm.01.L', 'f_middle.01.L', 'f_middle.02.L', 'f_middle.03.L', 'palm.02.L', 'f_ring.01.L', 'f_ring.02.L', 'f_ring.03.L', 'palm.03.L', 'f_pinky.01.L', 'f_pinky.02.L', 'f_pinky.03.L', 'palm.04.L', 'shoulder.R', 'upper_arm.R', 'upper_arm.R.001', 'forearm.R', 'forearm.R.001', 'hand.R', 'f_index.01.R', 'f_index.02.R', 'f_index.03.R', 'thumb.01.R', 'thumb.02.R', 'thumb.03.R', 'palm.01.R', 'f_middle.01.R', 'f_middle.02.R', 'f_middle.03.R', 'palm.02.R', 'f_ring.01.R', 'f_ring.02.R', 'f_ring.03.R', 'palm.03.R', 'f_pinky.01.R', 'f_pinky.02.R', 'f_pinky.03.R', 'palm.04.R'])
+    standard_rigify_deform_bones_substrings = ['spine.', 'lid.', 'lip.', 'brow.', 'teeth.', 'forehead.', 'heel.', 'ear.']
+
+    if not weight_armature or not anim_armature:
+        print("Armature not found in bpy.data.armatures")
+        return
+
+    for bone in weight_armature.bones:
+        if 'helper' not in bone.name:
+            if any(substring in bone.name for substring in standard_rigify_deform_bones_substrings):
+                standard_rigify_deform_bones.add(bone.name)
+
+    for obj in mesh_lists:
+        
+        if not obj.modifiers.get('Armature', False):
+            continue
+        
+        obj_aramture_modifier_object = obj.modifiers.get('Armature').object
+        
+        if rig_type == 'ANIMRIG':
+            
+            obj['_rig_name'] = 'rig_anim'
+            obj.modifiers.get('Armature').object = bpy.data.objects.get(obj['_rig_name'])
+            write_armature_data_to_custom_properties(obj)
+            
+            for deform_bone in standard_rigify_deform_bones:
+                if obj.vertex_groups.get(deform_bone):
+                    obj.vertex_groups.get(deform_bone).name = 'DEF-' + deform_bone
+            
+        elif rig_type == 'METARIG':
+            
+            obj['_rig_name'] = 'metarig'
+            obj.modifiers.get('Armature').object = bpy.data.objects.get(obj['_rig_name'])
+            write_armature_data_to_custom_properties(obj)
+            
+            for deform_bone in standard_rigify_deform_bones:
+                if obj.vertex_groups.get('DEF-' + deform_bone):
+                    obj.vertex_groups.get('DEF-' + deform_bone).name = deform_bone
--- mesh_weight_helper_to_parent().py
+++ mesh_weight_helper_to_parent().py
@@ -32,52 +32,84 @@ def add_weight_via_geometry_node(target_object, parent_bone_name, helper_bone_na
         bpy.ops.object.modifier_apply(modifier="_temp_HelperToParent")
 
 def get_helper_parent(armature, helper_bone_name):
-    bpy.ops.object.mode_set(mode='OBJECT')
+
     helper_bone = armature.data.bones.get(helper_bone_name)
-    print (helper_bone)
+    
+    # Print the bone for debugging
+   
+    
     if helper_bone:
         if 'helper' in helper_bone.name:
-            return get_helper_parent(armature, helper_bone.parent.name)
+            if helper_bone.parent:
+                return get_helper_parent(armature, helper_bone.parent.name)
+            else:
+                print(f"Bone '{helper_bone_name}' has no parent.")
+                return None
         else:
             return helper_bone
     else:
-        return
+        return None
 
 def merge_helper_to_parent(object, armature = None):
     if armature is None:
         try:
             armature = object.modifiers['Armature'].object
         except:
-            print(f"{object.name} has no armature")
+            raise print(f"{object.name} has no armature")
     vetex_group_collection = []
-    
+    vetex_group_collection_merged = []
     for vertex_group in object.vertex_groups: # Might look redundant but removing will cause a decoding error -> suspected cause -> geometry node function reorders vertex groups in memory (?) 
-        vetex_group_collection.append(vertex_group.name)
-    
-    for vertex_group in vetex_group_collection:
         if 'helper' in str(vertex_group):
-            helper_bone_name = vertex_group
-            if get_helper_parent(armature, helper_bone_name):
-                parent_bone_name = get_helper_parent(armature, helper_bone_name).name
-                add_weight_via_geometry_node(object, parent_bone_name, helper_bone_name)
+            vetex_group_collection.append(vertex_group.name)
+    
+    for helper_bone_name in vetex_group_collection:
+        
+        print(f"Activity: Getting Parent of {helper_bone_name} ")
+        parent_bone_name = get_helper_parent(armature, helper_bone_name).name        
+        
+        if parent_bone_name:
+            print(f"Activity: {helper_bone_name} to {parent_bone_name}")
+            if parent_bone_name.startswith('ORG-'): # just a temporary measure helper bones should be parented to the def vers
+                parent_bone_name = parent_bone_name.replace('ORG-', 'DEF-', 1)
+                
+            add_weight_via_geometry_node(object, parent_bone_name, helper_bone_name)
+            
+            vetex_group_collection_merged.append(helper_bone_name)
+    
+    missed_bones = set(vetex_group_collection) - set(vetex_group_collection_merged)
+    print(missed_bones)
+    
+#    if len(vetex_group_collection) > 0:
+#        merge_helper_to_parent(object, armature)
 
                 
-
 def clear_shapekeys(object):
     bpy.ops.object.mode_set(mode='OBJECT')
     bpy.context.view_layer.objects.active = object
     object.select_set(True)
-    bpy.ops.object.shape_key_remove(all=True)
+    
+    try:
+        bpy.ops.object.shape_key_remove(all=True)
+    except:
+        None
+    
     object.select_set(False)
 
 def extract_shape_keys_vertex_positons(object):
     bpy.ops.object.mode_set(mode='OBJECT')
     shape_keys_vertex_positons = {}
-    shape_keys = object.data.shape_keys.key_blocks
+    
+    try:
+        shape_keys = object.data.shape_keys.key_blocks
+    except:
+        return shape_keys_vertex_positons        
     
     for shape_key in shape_keys:
         if shape_key.name != "Basis": # we can just get rid of this but for safety so that the new basis doesnt get influenced by this code
             shape_keys_vertex_positons[shape_key.name] = [shape_key.data[i].co.copy() for i in range(len(shape_key.data))]
+        print(f"Activity: Extrated {shape_key.name} vertex locations")
+    
+    
     return shape_keys_vertex_positons
 
 def reconstruct_shape_keys_vertex_positons(object, vertex_data):
